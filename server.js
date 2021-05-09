1c43821dbefa8cac4e2fb109652cc5a23a5f1eda
66redBastard <66redbastard@gmail.com>
2021-05-09 20:26:09 +0300

    error handler connected with server, configjs updated

--- server.js
+++ server.js
@@ -2,10 +2,18 @@ require('rootpath')();
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const jwt = require('_helpers/jwt');
+const errorHandler = require('_helpers/error-handler');
 
 app.use(cors());
 app.use(express.json()); //Used to parse JSON bodies
 app.use(express.urlencoded()); //Parse URL-encoded bodies
+
+// use JWT auth to secure the api
+app.use(jwt());
+
+// global error handler
+app.use(errorHandler);
 
 // start server
 const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
